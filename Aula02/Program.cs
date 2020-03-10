f4153e0313e4e9013512caf991b340fb17754570
Adryan Alencar <ifsp.aluno@gmail.com>
2020-03-10 17:24:03 -0300

    Exercicios referente a Aula 02

---
+++ Aula02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bimestre01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = { "Adryan Alencar", "Beatriz Minha Linda<3" };
+
+            foreach (string item in nomes)
+            {
+                Console.WriteLine($"O nome na posição é {item}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
